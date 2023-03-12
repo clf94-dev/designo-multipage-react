03a443aabcf8345f3da04c11e9dfa1b115d773b9
clf94-dev <clf94@hotmail.es>
2023-03-12 08:11:20 +0100

    added assets & routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Locations from './pages/Locations';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/projects" element={<h1>Projects</h1>} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
 
